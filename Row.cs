926a970a9d8714140628910500a14af93ebdd2ce
CrocodileColombo <148744477+CrocodileColombo@users.noreply.github.com>
2023-11-01 18:01:44 +0500

    Add files via upload

---
+++ Row.cs
@@ -0,0 +1,17 @@
+﻿class Row
+{
+    int Id { get; set; }
+    int Index { get; set; }
+    string Region { get; set; }
+    public Row(int id, int index, string region)
+    {
+        this.Id = id;
+        this.Index = index;
+        this.Region = region;
+
+    }
+    public string Print()
+    {
+        return ($"В {this.Region} индекс загрязнения {this.Index}");
+    }
+}
